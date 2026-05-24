7183092049e5766e3a2ec74d9ba4b5eb59152f26
Vidya N <vidyavidya12543@gmail.com>
2026-05-24 20:08:42 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,5 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET || 'secret', {
@@ -22,7 +21,7 @@ const registerUser = async (req, res) => {
       name,
       email,
       password,
-      role: role || 'User',
+      role: role || 'Student',
     });
 
     if (user) {
@@ -47,7 +46,7 @@ const authUser = async (req, res) => {
 
     const user = await User.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
+    if (user && (await user.matchPassword(password))) {
       res.json({
         _id: user._id,
         name: user.name,
